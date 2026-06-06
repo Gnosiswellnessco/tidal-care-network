2b02cb4add29e8a7995bccb953e9181cf3e55fc1
gnosiswellnessco <info@gnosiswellnessco.com>
2026-06-06 16:59:24 -0400

    Add footer links: find a provider, how care works, report a concern

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,10 +13,13 @@ export default function Footer() {
             <img src={LOGO.full} alt="Tidal Care Network" style={{ height: 84, width: 'auto', display: 'block' }} />
           </Link>
           <nav style={{ display: 'flex', gap: 18, flexWrap: 'wrap', alignItems: 'center' }}>
+            <Link href="/directory" style={link}>Find a provider</Link>
             <Link href="/news" style={link}>News</Link>
+            <Link href="/how-care-works" style={link}>How care works</Link>
             <Link href="/terms-of-participation" style={link}>Provider Terms</Link>
             <Link href="/ethics" style={link}>Ethics Agreement</Link>
             <Link href="/privacy" style={link}>Privacy Policy</Link>
+            <Link href="/report-concern" style={link}>Report a concern</Link>
             <a href="mailto:info@tidalcare.org" style={link}>Contact</a>
           </nav>
         </div>
